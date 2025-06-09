39ef21985349193778d380d5780060f91aac4f30
= <=>
2025-06-09 00:16:45 -0300

    Style: imagens para teste.

---
+++ src/assets/imagens/cards/cards.tsx
@@ -0,0 +1,26 @@
+import imagem_48 from "./Frame_48.svg";
+import imagem_49 from "./Frame_49.svg";
+import imagem_50 from "./Frame_50.svg";
+import imagem_51 from "./Frame_51.svg";
+import imagem_52 from "./Frame_52.svg";
+import imagem_53 from "./Frame_53.svg";
+import imagem_54 from "./Frame_54.svg";
+import imagem_55 from "./Frame_55.svg";
+import imagem_56 from "./Frame_56.svg";
+import imagem_57 from "./Frame_57.svg";
+import imagem_58 from "./Frame_58.svg";
+import imagem_59 from "./Frame_59.svg";
+import imagem_60 from "./Frame_60.svg";
+import imagem_61 from "./Frame_61.svg";
+import imagem_62 from "./Frame_62.svg";
+import imagem_63 from "./Frame_63.svg";
+import imagem_64 from "./Frame_64.svg";
+import imagem_65 from "./Frame_65.svg";
+
+const imagens = [
+  imagem_48, imagem_49, imagem_50, imagem_51, imagem_52, imagem_53,
+  imagem_54, imagem_55, imagem_56, imagem_57, imagem_58, imagem_59,
+  imagem_60, imagem_61, imagem_62, imagem_63, imagem_64, imagem_65,
+];
+
+export default imagens;
