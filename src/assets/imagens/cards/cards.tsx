a8b7e35c788e710d0ffd1b0cf078dea86f8ee5aa
= <=>
2025-06-12 02:14:58 -0300

    Feat: mais imagens.

--- src/assets/imagens/cards/cards.tsx
+++ src/assets/imagens/cards/cards.tsx
@@ -16,11 +16,32 @@ import imagem_62 from "./Frame_62.svg";
 import imagem_63 from "./Frame_63.svg";
 import imagem_64 from "./Frame_64.svg";
 import imagem_65 from "./Frame_65.svg";
+import imagem_66 from "./Frame_66.svg";
+import imagem_67 from "./Frame_67.svg";
+import imagem_68 from "./Frame_68.svg";
+import imagem_69 from "./Frame_69.svg";
+import imagem_70 from "./Frame_70.svg";
+import imagem_71 from "./Frame_71.svg";
+import imagem_72 from "./Frame_72.svg";
+import imagem_73 from "./Frame_73.svg";
+import imagem_74 from "./Frame_74.svg";
+import imagem_75 from "./Frame_75.svg";
+import imagem_76 from "./Frame_76.svg";
+import imagem_77 from "./Frame_77.svg";
+import imagem_78 from "./Frame_78.svg";
+import imagem_79 from "./Frame_79.svg";
+import imagem_80 from "./Frame_80.svg";
+import imagem_81 from "./Frame_81.svg";
+import imagem_82 from "./Frame_82.svg";
+import imagem_83 from "./Frame_83.svg";
 
 const imagens = [
   imagem_48, imagem_49, imagem_50, imagem_51, imagem_52, imagem_53,
   imagem_54, imagem_55, imagem_56, imagem_57, imagem_58, imagem_59,
   imagem_60, imagem_61, imagem_62, imagem_63, imagem_64, imagem_65,
+  imagem_66, imagem_67, imagem_68, imagem_69, imagem_70, imagem_71,
+  imagem_72, imagem_73, imagem_74, imagem_75, imagem_76, imagem_77,
+  imagem_78, imagem_79, imagem_80, imagem_81, imagem_82, imagem_83
 ];
 
 export default imagens;
