4781e13543993d5de9d01364b841cf4b189d7169
= <=>
2025-07-17 22:47:11 -0300

    Refactor: Trocando imagens por novas.

---
+++ src/assets/imagens/cards/_cards.tsx
@@ -0,0 +1,35 @@
+import araucaria from "./araucaria.webp";
+import azulzinha from "./araucaria.webp";
+import caliandra_rosa from "./caliandra_rosa.webp";
+import cipo_de_sao_joao from "./cipo_de_sao_joao.webp";
+import gabiroba from "./gabiroba.webp";
+import inga from "./inga.webp";
+import jatoba from "./jatoba.webp";
+import lantana from "./lantana.webp";
+import manaca_de_cheiro from "./manaca_de_cheiro.webp";
+import orelha_de_onca from "./orelha_de_onca.webp";
+import palmeira_jucara from "./palmeira_jucara.webp";
+import pau_brasil from "./pau_brasil.webp";
+import pitangueira from "./pitangueira.webp";
+import primavera from "./primavera.webp";
+import vedelia from "./vedelia.webp";
+
+const imagens = [
+  { dadoImagem: araucaria, nome: "Araucária" },
+  { dadoImagem: azulzinha, nome: "Azulzinha" },
+  { dadoImagem: caliandra_rosa, nome: "Caliandra Rosa" },
+  { dadoImagem: cipo_de_sao_joao, nome: "Cipó de São João" },
+  { dadoImagem: gabiroba, nome: "Gabiroba" },
+  { dadoImagem: inga, nome: "Ingá" },
+  { dadoImagem: jatoba, nome: "Jatobá" },
+  { dadoImagem: lantana, nome: "Lantana" },
+  { dadoImagem: manaca_de_cheiro, nome: "Manacá de Cheiro" },
+  { dadoImagem: orelha_de_onca, nome: "Orelha de Onça" },
+  { dadoImagem: palmeira_jucara, nome: "Palmeira Juçara" },
+  { dadoImagem: pau_brasil, nome: "Pau Brasil" },
+  { dadoImagem: pitangueira, nome: "Pitangueira" },
+  { dadoImagem: primavera, nome: "Primavera" },
+  { dadoImagem: vedelia, nome: "Vedélia" },
+];
+
+export default imagens;
